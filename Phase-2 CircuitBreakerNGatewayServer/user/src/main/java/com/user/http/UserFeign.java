e245b0f66fcd95dfaf3c351812ebb9a602dc2f67
ajit <ajinathp.guru@gmail.com>
2026-08-28 20:51:39 +0530

    CircuitBreaker with Feign Client.

--- Phase-2 CircuitBreakerNGatewayServer/user/src/main/java/com/user/http/UserFeign.java
+++ Phase-2 CircuitBreakerNGatewayServer/user/src/main/java/com/user/http/UserFeign.java
@@ -1,11 +1,12 @@
 package com.user.http;
 
 import com.user.dto.HttpClientResponseDto;
+import com.user.fallback.AccountFallback;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name = "account", url = "http://localhost:1414")
+@FeignClient(name = "account", url = "http://localhost:1414", fallbackFactory = AccountFallback.class)
 public interface UserFeign {
     @GetMapping("/account/feign")
     public ResponseEntity<HttpClientResponseDto> getAccount();
